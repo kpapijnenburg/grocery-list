11388cbe7ac526007e371c32145e3f94b4f23a45
koenp <kpapijnenburg@gmail.com>
2019-02-06 14:17:01 +0100

    Added style changing when checking

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,40 +3,35 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 
 class Item extends Component {
+  state = {
+    isChecked: false
+  };
+
+  toggleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked
+    });
+  };
+
   render() {
+    let classes = "";
+    if (this.state.isChecked) classes = "checked";
+
     const { id, name, amount, onDelete, onIncrement, onDecrement } = this.props;
     return (
       <div>
-        <Checkbox color="default" />
-        {name}: {amount}
+        <Checkbox
+          onChange={this.toggleChange}
+          checked={this.state.isChecked}
+          color="default"
+        />
+        <span className={classes}>
+          {name}: {amount}
+        </span>
         <Button onClick={() => onIncrement(id)}> + </Button>
         <Button onClick={() => onDecrement(id)}> - </Button>
         <Button onClick={() => onDelete(id)}> X </Button>
       </div>
-      // <div>
-      //   <input type="checkbox" />
-      //   <label htmlFor="">
-      //     {this.props.name}: {this.props.amount}
-      //   </label>
-      //   <button
-      //     onClick={() => this.props.onIncrement(this.props.item)}
-      //     className="btn btn-secondary btn-sm m-2"
-      //   >
-      //     +
-      //   </button>
-      //   <button
-      //     onClick={() => this.props.onDecrement(this.props.item)}
-      //     className="btn btn-secondary btn-sm m-2"
-      //   >
-      //     -
-      //   </button>
-      //   <button
-      //     onClick={() => this.props.onDelete(this.props.item.id)}
-      //     className="btn btn-danger btn-sm m-2"
-      //   >
-      //     x
-      //   </button>
-      // </div>
     );
   }
 }
